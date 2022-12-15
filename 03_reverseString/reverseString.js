c863901785ed8493d54f1ed910d5d372a720ca9d
jacobaswanson <jakeaswan@yahoo.com>
2022-12-15 14:50:35 -0600

    Exercise 03 solved, committing solution'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let outputString = '';
+    for(let i = string.length; i >= 0; i--){
+        outputString += string.substr(i, 1);
+    }
+    return outputString;
 
 };
 

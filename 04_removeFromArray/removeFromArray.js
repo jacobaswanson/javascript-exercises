60ad968d67d82e5bff21e6bc9d35b86df970c6e7
jacobaswanson <jakeaswan@yahoo.com>
2022-12-15 19:52:17 -0600

    Exercise 04 Solved, committing solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    let final = [];
+    let match = false;
+    for(let i = 0; i < array.length; i++){
+        let testThis = array[i];
+        console.log(testThis);
+        let c = 1;
+        while(c < arguments.length ){
+            newArg = arguments[c];
+            if(testThis === newArg){
+                match = true;
+            }
+            console.log("testThis = " + testThis);
+            console.log("argument = " + newArg);
+            c++;
+        }
 
-};
+        if(match){
+        }else{
+            final.push(array[i]);
+        }
+        match = false;
+        }
+    return final;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

a6b8853fe57999e888946e041bf3c3a595497572
jacobaswanson <jakeaswan@yahoo.com>
2022-12-15 20:44:33 -0600

    more concise solution found, committing new solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,5 @@
 const leapYears = function(year) {
-    let leap = false;
-    if(year % 4 === 0){
-        if(year % 100 === 0){
-            if(year % 400 === 0){
-                leap = true;
-            }
-        }else{
-            leap = true;
-        }
-        }
-        return leap;
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
     }
 
 

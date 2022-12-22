69caf619b56d467298d8480a633df27ee42a3bc6
jacobaswanson <jakeaswan@yahoo.com>
2022-12-21 22:57:59 -0600

    Exercise 10 Solved, committing solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    let temp = 0;
+    let numbers = [1,1];
+    if(num < 1){
+        return 'OOPS';
+    }else{
+        for(let i = 1; i < num; i++){
+            numbers.push(numbers[i] + numbers[i - 1]);
+        }
+    return numbers[num - 1];
+    }
 };
 
 // Do not edit below this line

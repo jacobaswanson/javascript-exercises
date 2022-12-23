73224f7f13dd1862972526d123515497a41d0719
jacobaswanson <jakeaswan@yahoo.com>
2022-12-22 23:16:07 -0600

    Exercise 11 Solved, Committing Solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    let temp = {};
+    for(let i = 0; i < books.length; i++){
+        temp = books[i];
+        titles.push(temp["title"]);
+    }
+    return titles;
 };
 
 // Do not edit below this line
